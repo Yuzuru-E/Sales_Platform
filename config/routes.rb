170f7c9db497e6fff536084abad641fe0a51b9bd
Yuzuru-E <yzryzr88@gmail.com>
2020-12-09 15:10:02 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'matters#index'
+  devise_for :users
+
+  resources :matters
+
+  resources :users do
+    member do
+      get 'logout'
+    end
+  end
+  
 end
