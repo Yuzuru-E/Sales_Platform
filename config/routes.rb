4e7f3ea391c7fa78de9a3c6ea2d6b0a5491748d5
Yuzuru-E <yzryzr88@gmail.com>
2020-12-13 15:38:35 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   root 'matters#index'
   devise_for :users
 
-  resources :matters
+  resources :matters 
 
   resources :users do
     member do
       get 'logout'
+      get 'edit_cliant'
+      get 'edit_contractor'
     end
   end
   
