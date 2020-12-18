245267099bc73042cd199e845ac3cb621d897c74
Yuzuru-E <yzryzr88@gmail.com>
2020-12-18 15:08:44 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,35 @@ class UsersController < ApplicationController
   end
 
   def show
+    @client = Client.find_by(user_id: current_user.id)
+    @contractor = Contractor.find_by(user_id: current_user.id)
     @user = User.find(current_user.id)
-    if @user.image?
-      @userImage = @user.image.to_s
+    if @user.profile_image?
+      @userImage = @user.profile_image.to_s
     else
       @userImage = nil
     end
   end
 
-  def edit_client
+  def edit
+    @user = User.find(current_user.id)
+    if @user.profile_image?
+      @userImage = @user.profile_image.to_s
+    else
+      @userImage = nil
+    end
   end
 
-  def edit_contaractor
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(current_user.id), notice: 'ユーザー情報が更新されました。'
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :contact_mail, :profile, :profile_image, :industry_id)
   end
   
 end
