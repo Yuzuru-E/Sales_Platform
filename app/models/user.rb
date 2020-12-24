4c606530ae90e80be393d59bac2f887c8adeb787
Yuzuru-E <yzryzr88@gmail.com>
2020-12-24 11:42:30 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :industry
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :gender
-  belongs_to_active_hash :operating_status
-
+  has_many :clients
+  has_many :contractor
+  
   mount_uploader :profile_image, ImageUploader
 end
